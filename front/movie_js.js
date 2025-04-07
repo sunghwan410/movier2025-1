a5dd4bf3af437dc766f2312e7557b413b26e3b17
sungh <sunghwanssi@naver.com>
2025-04-07 14:25:54 +0900

    all 영화추천 완성

---
+++ front/movie_js.js
@@ -0,0 +1,49 @@
+let MovieObject = {
+    init: function() {
+            alert("init함수가 불려짐")
+    },
+
+    getall: function() {
+        alert("getall 함수가 불려짐")
+        $.ajax({
+        //실행할 코드
+        type: "GET",
+        url: "http://localhost:8000/all",
+        }).done(function(response){
+        //성공코드
+            console.log(response)
+            movielist = response.result
+
+            topdiv = document.createElement("div")
+            topdiv.style = "column-count:5"
+            document.body.appendChild(topdiv)
+
+            movielist.forEach( movie => {
+                cmovie = document.createElement("div")
+                cmovie.className = "card"
+    
+                mimg = document.createElement("img")
+                mimg.className = "card-img-top"
+                mimg.src = movie.poster_path
+                mimg.onclick = function(){
+                    window.open(movie.url)
+                }
+            mimg.onmouseover = function(){
+            mimg.style.cursor = "pointer"
+
+            }
+                cmovie.appendChild(mimg)
+                topdiv.appendChild(cmovie)
+            });
+
+
+
+        }).fail(function(error){
+        //실패코드
+            console.log(error)
+        });
+    }
+}
+
+MovieObject.init();
+MovieObject.getall();
